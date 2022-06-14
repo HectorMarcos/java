0f74bed3b297dcab64054b24f1f4a1a5daca09f8
hector <hectormarcx@gmail.com>
2022-06-14 23:56:11 +0200

    test

---
+++ test/daktika04/daktika04Test.java
@@ -0,0 +1,15 @@
+package daktika04;
+
+import org.junit.Test;
+
+public class daktika04Test {
+    @Test
+    public void ejercicio4() {
+        int num1 = 10; // set num1 equal 10
+        int num2 = 20; // set num2 equal 20
+        int sm = num1 + num2; // set sum equal num1 plus num2
+        System.out.println(sm);
+    }
+}
+
+
