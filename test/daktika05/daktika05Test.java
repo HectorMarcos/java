1ac3335dc64409506ddfbec6c2be80baed0b5ace
hector <hectormarcx@gmail.com>
2022-06-14 19:38:33 +0200

    tests

---
+++ test/daktika05/daktika05Test.java
@@ -0,0 +1,48 @@
+package daktika05;
+
+import org.junit.Test;
+
+public class daktika05Test {
+
+    @Test
+    public void ejercicio5() {
+        boolean bol = false; //
+        char xar = 'C';
+        byte bt = 10;
+        short sh= 20;
+        int i = 30;
+        long lng = 10000000;
+        float flt = 10.5F;
+        double dbl = 2.000000000052E10;
+
+        System.out.println(bol);
+        System.out.println(xar);
+        System.out.println(bt);
+        System.out.println(sh);
+        System.out.println(i);
+        System.out.println(lng);
+        System.out.println(flt);
+        System.out.println(dbl);
+    }
+
+    @Test
+    public void ejercicio6() {
+        boolean bol = false; //
+        char xar = 'C';
+        byte bt = 10;
+        short sh= 20;
+        int i = 30;
+        long lng = 10000000;
+        float flt = 10.5F;
+        double dbl = 2.000000000052E10;
+
+        System.out.println(bol);
+        System.out.println(xar);
+        System.out.println(bt);
+        System.out.println(sh);
+        System.out.println(i);
+        System.out.println(lng);
+        System.out.println(flt);
+        System.out.println(dbl);
+    }
+}
