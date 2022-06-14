3e8b27ad96d5310f74a0ee152b421aa5024aab0c
Hector Marcos <hectormarcx@gmail.com>
2022-06-14 17:49:06 +0200

    daktika04

--- src/daktika03/daktika03.java
+++ src/daktika03/daktika03.java
@@ -1,4 +1,12 @@
 package daktika03;
 
 public class daktika03 {
+    public static void main(String[] args) {
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello\tWorld");
+        System.out.println("Hello\"World");
+        System.out.println("Hello\'World");
+        System.out.println("Hello\rWorld");
+        System.out.println("Hello\bWorld");
+    }
 }
--- src/daktika04/daktika04.java
+++ src/daktika04/daktika04.java
@@ -1,4 +1,10 @@
 package daktika04;
 
 public class daktika04 {
+    public static void main(String[] args){
+        int num1 = 10; // set num1 equal 10
+        int num2 = 20; // set num2 equal 20
+        int sum = num1 + num2; // set sum equal num1 plus num2
+        System.out.println(sum);
+    }
 }
