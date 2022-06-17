bad80ab1d5e0b06245e988a846616a12fdfeeafc
Hector Marcos <hectormarcx@gmail.com>
2022-06-17 17:48:01 +0200

    java31

--- src/java30.java
+++ src/java30.java
@@ -1,3 +1,9 @@
-public class java30 {
+import static java30.main.bye;
+import static java30.main.wave;
 
+public class java30 {
+    public static void main(String[] args) {
+        wave();
+        bye();
+    }
 }
---
+++ src/java30/main.java
@@ -0,0 +1,12 @@
+package java30;
+
+public class main {
+    public static void wave(){
+        System.out.println("Hello there!");
+    }
+
+    public static void bye(){
+        System.out.println("See u there!");
+    }
+}
+
