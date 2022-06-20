dede2c49771aa5693b55f619b8e89c76b9a59004
hector <hectormarcx@gmail.com>
2022-06-20 22:14:39 +0200

    jdbc

--- src/MyJDBC.java
+++ src/MyJDBC.java
@@ -1,26 +1,37 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MyJDBC {
-
-    public static void main(String[] args) {
-
-        try{
-
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "admin");
-
-        Statement statement = connection.createStatement();
-
-        ResultSet resultSet = statement.executeQuery("select * from people");
-
-        while(resultSet.next()){
-            System.out.println(resultSet.getString("name"));
-        }
-
-        }catch (Exception e){
+    void selectAll(String table) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "admin");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from "+table);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+    void insert(String name) throws SQLException {
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "admin");
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate("INSERT INTO `jdbc`.`people`(`name`)VALUES('"+name+"')");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        MyJDBC myjdbc = new MyJDBC();
+        myjdbc.insert("Pedro");
+        myjdbc.selectAll("people");
+
+    }
 }
+
+
+//FIXME https://www.tutorialspoint.com/jdbc/jdbc-insert-records.htm
